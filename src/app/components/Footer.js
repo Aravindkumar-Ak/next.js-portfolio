224b58f0a0fa5073be7b15633724bb9aede1a03e
Aravindkumar <Aravindkumarak1147@gmail.com>
2024-02-17 20:00:15 +0530

    updated error

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='dark:bg-black'><footer className='text-center text-md p-4 text-pink-400 pt-8 flex justify-around items-center'>
-    <p>&copy;Design and Developed By <span className='text-teal-400 font-Rampart '>Aravindkumar</span></p>
+    <p>Design and Developed By <span className='text-teal-400 font-Rampart '>Aravindkumar</span></p>
     <p className='font-semibold '><a href="mailto:hege@example.com">Email:Aravindkumarak1147@gmail.com</a></p>
   </footer></div>
   )
--- src/app/components/FormModal.js
+++ src/app/components/FormModal.js
@@ -5,7 +5,7 @@ const FormModal = ({setModalOpen}) => {
     <div className="flex flex-col items-center justify-center w-3/4 bg-black  mx-auto rounded-lg p-8 text-center">
         <h1 className="text-white text-lg md:text-3xl font-bold">Get in Touch</h1>
         <p className="text-white mt-4">
-          I'm open to hearing about new opportunities and collaborations. Feel
+          I am open to hearing about new opportunities and collaborations. Feel
           free to ask your doubts, questions, or just say hi.
         </p>
 
