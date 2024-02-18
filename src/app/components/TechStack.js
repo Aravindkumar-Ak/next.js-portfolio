4bce8a374dfd6a0d7c9b3828239590db72dace02
Aravindkumar <Aravindkumarak1147@gmail.com>
2024-02-18 15:48:36 +0530

    techstack updated

--- src/app/components/TechStack.js
+++ src/app/components/TechStack.js
@@ -15,37 +15,37 @@ const TechStack = () => {
      <h1 className='font-semibold text-3xl text-teal-400  border-b-2 border-blue-400 inline-block'>TECH STACK</h1>
 <div className='flex md:flex-row flex-col justify-evenly gap-8 items-center mt-20'>
     <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer'>
-        <h1>Java</h1>
+       
         <Image src={web1} alt="pic"/>
     </div>
     <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer'>
-        <h1>SpringBoot</h1>
+     
         <Image src={web2} alt="pic"/>
     </div>
     <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer '>
-        <h1>Sql</h1>
+   
         <Image src={web3} alt="pic"/>
     </div>
     <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer'>
-        <h1>Mysql</h1>
+        
         <Image src={web4} alt="pic"/>
     </div>
 </div>
 <div className='flex md:flex-row flex-col justify-evenly gap-20 items-center mt-20'>
 <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer'>
-        <h1>Html</h1>
+        
         <Image src={web5} alt="pic"/>
     </div>
     <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer'>
-        <h1>Css</h1>
+       
         <Image src={web6} alt="pic"/>
     </div>
     <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer'>
-        <h1>Javascript</h1>
+      
         <Image src={web7} alt="pic"/>
     </div>
     <div className='w-[150px] hover:-translate-y-4 hover:duration-300 cursor-pointer'>
-        <h1>React</h1>
+      
         <Image src={web8} alt="pic"/>
     </div>
 </div>
